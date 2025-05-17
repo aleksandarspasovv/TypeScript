65815876e96b689eeebc2abd5db0b8131044363a
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2025-05-17 19:38:51 +0300

    add: exercises_03.ts

--- Introduction/exercises_03.ts
+++ Introduction/exercises_03.ts
@@ -1,1 +1,45 @@
+interface User {
+    name: string,
+    isAdmin: boolean,
+}
 
+const users: User[] = [
+    {name: 'Ivan', isAdmin: false},
+    {name: 'Alex', isAdmin: true},
+]
+
+function getIsAdmin(users: User[]): User[] {
+    return users.filter(user => user.isAdmin);
+}
+
+type Currency = "USD" | 'EUR' | 'BGN';
+
+function convert(amount: number, currency: Currency): string {
+    return `${amount} ${currency}`;
+}
+
+function inValidEmail(email: string): boolean {
+    const atIndex = email.indexOf('@');
+    const dotIndex = email.indexOf('.');
+    return atIndex > 0 && dotIndex > atIndex;
+}
+
+interface ApiResposne<T> {
+    data: T;
+    success: boolean;
+    error: string | null;
+}
+
+const respose: ApiResposne<string[]> = {
+    data: ['item1', 'item2'],
+    success: true,
+    error: null,
+}
+
+interface Product {
+    id: string,
+    price: number,
+
+}
+
+function mapById(products: Product[], )
