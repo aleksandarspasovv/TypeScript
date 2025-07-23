2c30316e7de94d46cb092b4bcfdb919b58ee8722
unknown <777alexander.spasov@gmail.com>
2025-07-24 02:38:43 +0300

    add: TS drawings

--- Advanced/objectExplanation.ts
+++ Advanced/objectExplanation.ts
@@ -7,3 +7,22 @@ function createUser2({name: string, age: nuber}) {
 let newUser = {name: 'Alex', age: 29, email: 'a@a.bg'};
 
 createUser2(newUser)          //TS allows passing of a 3rd parameter email, which is wrong
+
+
+
+
+// corrent way writing
+
+type User = {
+    name: string,
+    email: string,
+    isActive: boolean
+}
+
+function createUserCorrect(user: User): string {
+    // logic for creation of the user
+    // if function returns the User, then it must
+    // return all the values of the paramenters
+    return `User was created successfully`
+}
+
