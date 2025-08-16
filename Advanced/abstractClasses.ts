89bc82b05af84ba825f513bf576b004590b7e3bd
unknown <777alexander.spasov@gmail.com>
2025-08-16 06:32:34 +0300

    add: added abstactClasses.ts defenitions

--- Advanced/abstractClasses.ts
+++ Advanced/abstractClasses.ts
@@ -3,10 +3,23 @@ abstract class TakePhoto {
         public cameraMode: string,
         public filter: string,
     ){}
+
+    abstract getSepia(): void // needs to have abstract so that no definition is provided
 }
 
 class Instagram2 extends TakePhoto{
 
+    constructor(
+        public cameraMode: string,
+        public filter: string,
+        public burst: string   // adding to the constructor
+    ){
+        super(cameraMode, filter)
+    }
+
+    getSepia(): void {
+        console.log('method implemented')
+    }
 }
 
-const al = new Instagram2('normal', 'none')
+const al = new Instagram2('normal', 'none', 'noBurst')
