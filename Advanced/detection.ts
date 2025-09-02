1b1190fe8126b859177b26a0361d3438669e130f
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2025-09-02 23:00:24 +0300

    add: detections.ts

---
+++ Advanced/detection.ts
@@ -0,0 +1,36 @@
+interface User22 {
+    name: string,
+    age: number
+}
+
+interface Admin22 {
+    name: string,
+    age: number,
+    isAdmin: boolean
+}
+
+function isAdminCheck(account: User22 | Admin22){
+    if ('isAdmin' in account){
+        return account.isAdmin
+    }
+}
+
+type Fish = {
+    swim: () => void
+}
+
+type Bird = {fly: () => void}
+
+function isFish(pet: Bird | Fish): pet is Fish{       //pet is Fish
+    return (pet as Fish).swim !== undefined
+}
+
+function getFood(pet: Fish | Bird){
+    if (isFish(pet)){
+        pet
+        return 'fish food'
+    } else {
+        pet
+        return 'bird food'
+    }
+}
